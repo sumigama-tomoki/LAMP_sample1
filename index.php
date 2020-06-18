a316da8e1e2aa654eab32ca7fd11d6d1aa24a756
sumigama-tomoki <63226200+sumigama-tomoki@users.noreply.github.com>
2020-06-18 12:41:51 +0900

    index

--- index.php
+++ index.php
@@ -1,12 +1,33 @@
 <?php
-$dsn = 'mysql:dbname=user;host=127.0.0.1;';
+$dsn = 'mysql:dbname=sample_db;host=localhost;';
 $user = 'root';
 $password = 'sagami893';
 try {
     $dbh = new PDO($dsn, $user, $password);
-    echo "接続成功\n";
+​
+    $sql = "select * from user";
+    $result = $dbh->query($sql);
 } catch (PDOException $e) {
     echo "接続失敗: " . $e->getMessage() . "\n";
     exit();
 }
 ?>
+​
+<!DOCTYPE html>
+<html lang="ja">
+​
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>LAMP SAMPLE PAGE</title>
+</head>
+​
+<body>
+    <div class="container">
+        <?php foreach ($result as $value) {
+            echo "<h1>$value[id] $value[name] $value[age]</h1>";
+        } ?>
+    </div>
+</body>
+​
+</html>
